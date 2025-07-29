a65f8c4e66ccd1fb9a4b66501d6a0fcdd7ea38c2
Joe <osmb1000@gmail.com>
2025-07-29 09:12:46 +0100

    initial commit

---
+++ AIOWoodcuttingGuild/src/com/osmb/script/woodcutting/data/PixelProvider.java
@@ -0,0 +1,41 @@
+package com.osmb.script.woodcutting.data;
+
+import com.osmb.api.visual.SearchablePixel;
+import com.osmb.api.visual.color.ColorModel;
+import com.osmb.api.visual.color.tolerance.impl.SingleThresholdComparator;
+
+public class PixelProvider {
+
+    /**
+     * Pixels representing a cluster of tree colors in HSL format. Will cover normal trees, oaks.
+     */
+     static final SearchablePixel[] TREE_CLUSTER_1 = new SearchablePixel[] {
+            new SearchablePixel(-14012413, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-12958706, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-11316685, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-13222121, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-11312366, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-13089777, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-12103646, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-15329787, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-15131126, new SingleThresholdComparator(2), ColorModel.HSL),
+    };
+    /**
+     * Pixels representing a cluster of tree colors in HSL format. Will cover maple trees.
+     */
+    static final SearchablePixel[] TREE_CLUSTER_2 = new SearchablePixel[] {
+            new SearchablePixel(-7247872, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-11395072, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-11260160, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-9619968, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-13495040, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-11518464, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-13821184, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-12900096, new SingleThresholdComparator(2), ColorModel.HSL),
+            new SearchablePixel(-8893696, new SingleThresholdComparator(2), ColorModel.HSL),
+    };
+
+    static final SearchablePixel[] AXE_PIXELS = new SearchablePixel[]{
+            new SearchablePixel(-8758767, new SingleThresholdComparator(1), ColorModel.HSL)
+    };
+}
